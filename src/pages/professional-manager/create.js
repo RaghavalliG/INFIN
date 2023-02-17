044609beb054c198bd0b2e2b1f0ea02ab7dc3666
bbasak-brainiumin <biswajit.basak@brainiuminfotech.com>
2023-02-17 22:20:59 +0530

    push the css for fixing some issue

--- src/pages/professional-manager/create.js
+++ src/pages/professional-manager/create.js
@@ -1,3 +1,4 @@
+import AdminFooter from "@/components/common/admin-footer";
 import AdminHeader from "@/components/common/admin-header";
 import Footer from "@/components/common/footer";
 import ProfessionalManagerCard from "@/components/professional-manager-card";
@@ -7,6 +8,7 @@ import SideMenu from "@/components/sideMenu";
 
 export default function ProfessionalManagerCreate() {
     return <>
+    <div className="admin_root_wrap">
     <div className='admin_wrap container-fluid'>
           <SideMenu />
           <div className="admin-content-area">
@@ -16,6 +18,7 @@ export default function ProfessionalManagerCreate() {
             </div>
           </div>
         </div>
-        <Footer />
+        <AdminFooter />
+        </div>
     </>
   }
--- src/pages/professional-manager/index.js
+++ src/pages/professional-manager/index.js
@@ -1,11 +1,13 @@
 import AdminFooter from "@/components/common/admin-footer";
 import AdminHeader from "@/components/common/admin-header";
+import Header from "@/components/common/header";
 import ProfessionalManagerCard from "@/components/professional-manager-card";
 import SideMenu from "@/components/sideMenu";
 
 
 export default function ProfessionalManager() {
     return <>
+    <div className="admin_root_wrap">
     <div className='admin_wrap container-fluid'>
           <SideMenu />
           <div className="admin-content-area">
@@ -17,5 +19,6 @@ export default function ProfessionalManager() {
           </div>
         </div>
         <AdminFooter />
+        </div>
     </>
   }
